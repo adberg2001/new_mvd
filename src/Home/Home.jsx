5a7ddbcdf3aabc796914849be55bf3ad504f9772
adylbek <aaaadyl.erg@gmail.com>
2021-08-06 08:31:11 +0600

    finish rubrics and footer layout

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from './components/Hero';
 import NewsListSection from './components/NewsListSection';
+import Rubrics from "./components/Rubrics";
+import Footer from "../Layout/Footer";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <NewsListSection />
+      <Rubrics />
+      <Footer/>
     </>
   );
 }
