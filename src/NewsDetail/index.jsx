0987945099e7aeb32c3fb23ce15f004d3b2c480a
whytdan <aktan.r.a@gmail.com>
2021-08-09 18:19:59 +0600

    Refactor all pages layout

--- src/NewsDetail/index.jsx
+++ src/NewsDetail/index.jsx
@@ -1,15 +1,12 @@
-import React, {useEffect, useState} from "react";
-import "swiper/swiper.min.css";
-import "swiper/components/navigation/navigation.min.css"
-import cls from "./NewsDetail.module.scss";
-import {Swiper, SwiperSlide} from "swiper/react";
-
+import React, { useEffect, useState } from 'react';
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import cls from './NewsDetail.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 // import Swiper core and required modules
-import SwiperCore, {
-  Navigation
-} from 'swiper/core';
-import Image from "next/image";
+import SwiperCore, { Navigation } from 'swiper/core';
+import Image from 'next/image';
 
 // install Swiper modules
 SwiperCore.use([Navigation]);
@@ -19,40 +16,50 @@ function formatDate(notFormattedDate) {
     d = date.getDate(),
     m = date.getMonth(),
     y = date.getFullYear();
-  return `${d.toString().padStart(2, "0")}.${m.toString().padStart(2, "0")}.${y}`
+  return `${d.toString().padStart(2, '0')}.${m
+    .toString()
+    .padStart(2, '0')}.${y}`;
 }
 
-export default function NewsDetailComponent({date, news_type, title, images, description}) {
+export default function NewsDetailComponent({
+  date,
+  news_type,
+  title,
+  images,
+  description,
+}) {
   const [data, setData] = useState(null);
   useEffect(() => {
-    setData(images)
-  }, [images, setData])
+    setData(images);
+  }, [images, setData]);
 
   return (
     <section className={`${cls.news_detail} container`}>
-      <div className={`${cls.news_detail__head} d-flex justify-content-between`}>
+      <div
+        className={`${cls.news_detail__head} d-flex justify-content-between`}
+      >
         <p className={`m-0`}>{formatDate(date)}</p>
         <p className={`m-0`}>{news_type}</p>
       </div>
 
-      <h2 className={`sections__heading`}>{title}</h2>
+      <h2 style={{ textAlign: 'start' }} className={`section__heading`}>
+        {title}
+      </h2>
 
       <div className={`${cls.news_detail__swiper_wrapper}`}>
         <Swiper navigation={true}>
-          {
-            data && data.map(({id, image}) => (
+          {data &&
+            data.map(({ id, image }) => (
               <SwiperSlide key={id}>
-                <div className={cls.news_detail__swiper_img}>
-                  <Image src={image} alt={image} layout='fill'/>
-                </div>
+                <div
+                  style={{ backgroundImage: `url('${image}')` }}
+                  className={cls.news_detail__swiper_img}
+                ></div>
               </SwiperSlide>
-            ))
-          }
+            ))}
         </Swiper>
       </div>
-      <div className={cls.news_detail__description}>
-        {description}
-      </div>
+      <div className={cls.news_detail__description}>{description}</div>
     </section>
-  )
+  );
 }
--- src/NormativeBase/index.jsx
+++ src/NormativeBase/index.jsx
@@ -1,61 +1,127 @@
-import React, {useState} from "react";
-import cls from "./NormativeBase.module.scss";
-import BaseSheetRow from "../components/BaseSheetRow";
+import React, { useState } from 'react';
+import cls from './NormativeBase.module.scss';
+import BaseSheetRow from '../components/BaseSheetRow';
+import Pagination from '@material-ui/lab/Pagination';
 
 const normativeBase = [
-  {id: "normative-base-1", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-2", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-3", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-4", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-5", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-6", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-7", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-8", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-9", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-11", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-12", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-13", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-14", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-15", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-16", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-17", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-18", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-  {id: "normative-base-19", label: "Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd", viewsCount: "2000"},
-]
+  {
+    id: 'normative-base-1',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-2',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-3',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-4',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-5',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-6',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-7',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-8',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-9',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-11',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-12',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-13',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-14',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-15',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-16',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-17',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-18',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+  {
+    id: 'normative-base-19',
+    label: 'Lorem ipsum dolor sit amment adnasdhn absdjabsda bhjabsd',
+    viewsCount: '2000',
+  },
+];
 
 export default function NormativeBaseComponent() {
-  const [activePag, setActivePag] = useState('1')
+  const [activePag, setActivePag] = useState('1');
 
   return (
     <section className={`${cls.main} container`}>
       <div className={cls.head}>
-        <h2 className={'sections__heading'}>Нормативная база</h2>
+        <h2 className={'section__heading'}>Нормативная база</h2>
         <p>
-          ЦЕНТРАЛИЗОВАННЫЙ БАНК ДАННЫХ <br/>
-          ПРАВОВОЙ ИНФОРМАЦИИ <br/>
+          ЦЕНТРАЛИЗОВАННЫЙ БАНК ДАННЫХ <br />
+          ПРАВОВОЙ ИНФОРМАЦИИ <br />
           КЫРГЫЗСКОЙ РЕСПУБЛИКИ
         </p>
       </div>
       <ul className={cls.normativeBase}>
-        {normativeBase.map(vacancy => <BaseSheetRow key={vacancy.id} {...vacancy}/>)}
-        <li style={{gridGap: 10}} className={`pagination d-flex justify-content-center align-items-center`}>
-          <button
-            onClick={() => setActivePag('1')} className={`${activePag === "1" ? 'active' : ""}`}
-          >
-            1
-          </button>
-          <button
-            onClick={() => setActivePag('2')} className={`${activePag === "2" ? 'active' : ""}`}
-          >
-            2
-          </button>
-          <button
-            onClick={() => setActivePag('3')} className={`${activePag === "3" ? 'active' : ""}`}
-          >
-            3
-          </button>
-        </li>
+        {normativeBase.map((vacancy) => (
+          <BaseSheetRow key={vacancy.id} {...vacancy} />
+        ))}
       </ul>
+
+      <Pagination
+        color="primary"
+        size="large"
+        page={1}
+        count={3}
+        className={cls.Pagination}
+      />
     </section>
-  )
+  );
 }
