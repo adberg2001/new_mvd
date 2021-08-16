11f4d76bef5ae7ce20b6d549aed876b736efd925
whytdan <aktan.r.a@gmail.com>
2021-08-17 05:28:54 +0600

    change eslint rule and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: false,
+  distDir: "build",
   images: {
     domains: ["164.90.170.123"],
   },
--- src/Home/components/Hero/index.jsx
+++ src/Home/components/Hero/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
-import cls from './Hero.module.scss';
-import HeroSlider from '../HeroSlider';
+import cls from "./Hero.module.scss";
+import HeroSlider from "../HeroSlider";
 
 export default function Hero({ on_main_slider }) {
   return (
